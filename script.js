9e3d543a8309241d0294e1fb866c41f2ebff1541
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-16 15:48:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const extendHex = (shortHex) => {
-  // write your code here
+  shortHex = shortHex.replace(/^#/, '').toUpperCase();
+
+  // If the shortHex has only one character per color, repeat each character
+  if (shortHex.length === 3) {
+    shortHex = shortHex
+      .split('')
+      .map(char => char + char)
+      .join('');
+  }
+
+  // Add # prefix and return the extended hex code
+  return `#${shortHex}`;
 };
 
 // Do not change the code below.
